207deda44f6bfcb6e566a2a9a7a2d9fd03d21e9a
microkbcrt <1811838153@qq.com>
2025-10-24 13:25:08 +0800

    Update get_icon_weather.py

--- get_icon_weather.py
+++ get_icon_weather.py
@@ -30,10 +30,10 @@ def get_weather_data():
     """
     grid_points = {
         'center': {'lat': -66.71, 'lon': 94.8}, # 广州市中心
-        'north':  {'lat': 23.40, 'lon': 113.22}, # 北部 (花都)
-        'south':  {'lat': 22.78, 'lon': 113.53}, # 南部 (南沙)
-        'east':   {'lat': 23.29, 'lon': 113.82}, # 东部 (增城)
-        'west':   {'lat': 23.17, 'lon': 112.89}  # 西部 (佛山三水)
+        'north':  {'lat': -66.00, 'lon': 95.0}, # 北部 (花都)
+        'south':  {'lat': -66.30, 'lon': 94.5}, # 南部 (南沙)
+        'east':   {'lat': -67.00, 'lon': 94.0}, # 东部 (增城)
+        'west':   {'lat': -67.20, 'lon': 95.5}  # 西部 (佛山三水)
     }
     
     base_url = "https://api.open-meteo.com/v1/forecast"
