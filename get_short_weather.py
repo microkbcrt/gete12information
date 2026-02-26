2a04c9c7cc3e6c39648223d84a50ec0f23e8fea5
microkbcrt <1811838153@qq.com>
2026-02-26 18:39:36 +0800

    Change default publisher from '邵福睿' to '陶瓷'

--- get_short_weather.py
+++ get_short_weather.py
@@ -25,7 +25,7 @@ try:
     
     # 提取需要的字段
     weather_data = {
-        "publisher": data.get('publishr', '邵福睿'),
+        "publisher": data.get('publishr', '陶瓷'),
         "forecast": data.get('forecast', '暂无最新预报'),
         "rtime": data.get('rtime', '刚刚')
     }
