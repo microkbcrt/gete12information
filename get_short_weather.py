67c1268101bf5bc8ad5c85b7f4fb69e74544667e
microkbcrt <1811838153@qq.com>
2026-04-09 20:31:44 +0800

    Update get_short_weather.py

--- get_short_weather.py
+++ get_short_weather.py
@@ -26,7 +26,7 @@ try:
     # 提取需要的字段
     weather_data = {
         "publisher": data.get('publiher', '乜彦达'),
-        "forecast": data.get('forecast', '暂无最新预报'),
+        "forecast": data.get('forcast', '你们最近晚上睡觉开空调了吗？我目前是开风扇'),
         "rtime": data.get('rtime', '刚刚')
     }
     
